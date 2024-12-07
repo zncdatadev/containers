a63604094464e79b164b7b7ec380924a34ab6c53
whg517 <huagang517@126.com>
2024-12-07 14:57:18 +0800

    build: refactor to use docker back build image (#113)
    * ci: fix some job missing value
    
    * build: refactor to use docker back build image
    
    * ci: refactor diff script and workflow
    
    * build: fix multi-arch err when bake --load
    
    * build: add cache from registry
    
    * refactor: enable all product to built
    
    * fix: fix superset build err
    
    * build: fix target arg err
    
    * fix: fix airflow build err
    
    * build: fix add dep product ver to args
    
    * ci: disable ci debug
    
    * ci: update products
    
    * fix: hbase miss thirdparty version

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -3,140 +3,52 @@
 set -e
 set -o pipefail
 
-CONTAINER_TOOL=${CONTAINER_TOOL:-"docker"}
-SIGNER_TOOL=${SIGNER_TOOL:-"cosign"}
+
 CI_DEBUG=${CI_DEBUG:-false}
+REGISTRY=${REGISTRY:-"quay.io/zncdatadev"}
+KUBEDOOP_VERSION=${KUBEDOOP_VERSION:-"0.0.0-dev"}
+KUBEDOOP_TAG="kubedoop${KUBEDOOP_VERSION}"
 
 PATH=$PATH:$(pwd)/bin
 
-. .scripts/lib.sh
 
-# if CI_DEBUG is set, then enable debug mode
+# If CI_DEBUG is set, enable debug mode
 if [ "$CI_DEBUG" = "true" ]; then
   set -x
 fi
 
 
-# Build a signal image
-function build_image () {
+function main () {
   local usage="
-Usage: command <Arguments> [OPTIONS] PATH
+Usage: command [OPTIONS][TARGET...]
 
-Build the image
+Build the image.
 
-Arguments:
-  -t, --tag string            Set the tag for the image
+Use docker buildx bake to build the image, and push the image to the registry.
 
 Options:
-  -h, --help                  Show this message
-  -d, --debug                 Build in debug mode
-  -f, --file string           Set the Dockerfile path, default is Dockerfile
-      --push                  Push the built image to the registry
-      --build-arg sring       Pass build arguments to the image, can be used multiple times
-      --platform string       Set the platform for the image, default is linux/amd64,linux/arm64.
-"
-  local tag
-  local dockerfile=""
-  local platform=""
-  local push=false
-  local build_args=()
-  local sign=false
-
-  while [ "$1" != "" ]; do
-    case $1 in
-      -t | --tag )
-        shift
-        tag=$1
-        ;;
-      -f | --file )
-        shift
-        dockerfile=$1
-        ;;
-      --platform )
-        shift
-        platform=$1
-        ;;
-      --push )
-        push=true
-        ;;
-      --build-arg )
-        shift
-        build_args+=($1)
-        ;;
-      --context )
-        shift
-        context=$1
-        ;;
-      -h | --help )
-        echo "$usage"
-        exit 0
-        ;;
-      * )
-        if [ -n "$context" ]; then
-          echo "Invalid argument: $1"
-          exit 1
-        fi
-        context=$1
-        ;;
-    esac
-    shift
-  done
-
-  if [ -z "$tag" ]; then
-    echo "Tag is required" >&2
-    exit 1
-  fi
-
-  system_requirements $sign
-
-  if ! build_sign_image "$CONTAINER_TOOL" "$tag" "$dockerfile" "$platform" $push "$build_args" "$context"; then
-    echo "ERROR: Failed to build image" >&2
-    exit 1
-  fi
-}
-
-
-# Build the product image
-function build_product () {
-  local usage="
-Usage: command [OPTIONS] PATH
-
-Build the product image
-
-Options:
-  -h, --help                      Show this message
-  -f, --file string               Set the Dockerfile path, default is Dockerfile
-      --product-version string    Set the product version
-      --push                      Push the built image to the registry
-      --platform string           Set the platform for the image, default is linux/amd64,linux/arm64.
-      --sign                      Sign the image, currently only support cosign
+  -h,  --help                     Show this message
+  -d,  --debug                    Build in debug mode
+  -p,  --push                     Push the built image to the registry
+  -s,  --sign                     Sign the image with cosign
 "
 
-  local context
-  local dockerfile=""
+  local debug=false
   local push=false
-  local platform=""
-  local product_version=""
   local sign=false
+  # Change target to array
+  declare -a targets=()
 
+  # Parse arguments
   while [ "$1" != "" ]; do
     case $1 in
-      -f | --file )
-        shift
-        dockerfile=$1
+      -d | --debug )
+        debug=true
         ;;
-      --push )
+      -p | --push )
         push=true
         ;;
-      --platform )
-        shift
-        platform=$1
-        ;;
-      --product-version )
-        shift
-        product_version=$1
-        ;;
-      --sign )
+      -s | --sign )
         sign=true
         ;;
       -h | --help )
@@ -144,76 +56,51 @@ Options:
         exit 0
         ;;
       * )
-        # if context already set, then exit
-        if [ -n "$context" ]; then
-          echo "Invalid argument: $1"
+        # Handle non-option argument as target
+        if [[ $1 != -* ]]; then
+          targets+=("$1")
+        else
+          echo "Error: Invalid argument '$1'"
+          echo "$usage"
           exit 1
         fi
-        context=$1
         ;;
     esac
     shift
   done
 
-  if [ -z "$context" ]; then
-    echo "Context is required"
-    exit 1
+  # Print all specified targets
+  if [ ${#targets[@]} -gt 0 ]; then
+    echo "INFO: Specified targets: ${targets[*]}"
   fi
 
+  # Check system requirements if signing is requested
   system_requirements $sign
 
-  local result
-  if ! result=$(get_product_metadata "$context" "$product_version"); then
-    echo "ERROR: Failed to get product metadata" >&2
-    exit 1
+
+  # Note:
+  # If push is not true, we use single-architecture to build the image.
+  # The docker version in Ubuntu 24.04 of the current GitHub runner does not
+  # support multi-architecture image building with the --load parameter.
+  # Moreover, if push is false, we do not need to build multi-architecture images,
+  # just load the current system architecture image into docker with the --load parameter.
+  local platforms='["linux/amd64", "linux/arm64"]'
+  if [ "$push" = false ]; then
+    platforms='["linux/'$(uname -m)'"]'
   fi
 
-  for item in $(echo $result | jq -c '.[]'); do
-    local tag=$(echo $item | jq -r '.tag')
+  # Get the bakefile configuration
+  local bakefile=$(get_bakefile $platforms)
 
-    local build_args=()
-    if echo $item | jq -e '.build_args?' > /dev/null; then
-      build_args=$(echo $item | jq -r '.build_args[]' | tr '\n' ' ')
-    fi
+  # Check debug mode
+  if [ "$debug" = true ] || [ "$CI_DEBUG" = "true" ]; then
+    debug=true
+  fi
 
-    build_sign_image "$CONTAINER_TOOL" "$tag" "$dockerfile" "$platform" $push "$build_args" "$context" $sign
-  done
+  # Update function call order of parameters
+  build_sign_image "$bakefile" $push $sign $debug "${targets[*]}"
 }
 
-function main () {
-  local usage="
-Usage: build.sh COMMAND [OPTIONS]
-
-Build image
-
-Commands:
-  image                       Build the image
-  product                     Build the product image
-
-Options:
-  -h, --help                  Show this message
-
-"
-  local command=$1
-  shift
-
-  case $command in
-    image )
-      build_image "$@"
-      ;;
-    product )
-      build_product "$@"
-      ;;
-    -h | --help )
-      echo "$usage"
-      exit 0
-      ;;
-    * )
-      echo "$usage"
-      exit 1
-      ;;
-  esac
-}
 
 function system_requirements () {
   local sign=$1
@@ -223,17 +110,210 @@ function system_requirements () {
     exit 1
   fi
 
-  if ! command -v $CONTAINER_TOOL > /dev/null; then
-    echo "$CONTAINER_TOOL is required!"
+  if ! command -v yq > /dev/null; then
+    echo "yq is required. Please install it refer to <https://mikefarah.gitbook.io/yq/>" >&2
     exit 1
   fi
 
-  if [ "$sign" = true ] && ! command -v $SIGNER_TOOL > /dev/null; then
-    echo "$SIGNER_TOOL is required, Please install it refer to <https://docs.sigstore.dev/cosign/system_config/installation/>" >&2
+  if [ "$sign" = true ] && ! command -v cosign > /dev/null; then
+    echo "cosignz is required, Please install it refer to <https://docs.sigstore.dev/cosign/system_config/installation/>" >&2
     exit 1
   fi
 }
 
 
-system_requirements
+# https://github.com/sigstore/cosign/issues/587
+# Use Cosign for keyless image signing
+function sign_image () {
+  local image=$1
+  local upload=$2
+
+  if [ $upload = true ]; then
+    cosign sign -y $image
+  fi
+}
+
+
+# Build image with docker bake
+# Arguments:
+#  $1: str   bakefile, docker bake file content, JSON object
+#  $2: bool  push, if true, push image to registry, else load to local docker, default is false
+#  $3: bool  sign, if true, sign image with cosign, default is false
+#  $4: bool  debug, if true, print debug info, default is false
+#  $5: str   targets, target names in bakefile, space separated
+# Returns:
+#  None
+function build_sign_image () {
+  local bakefile=$1
+  local push=$2
+  local sign=$3
+  local debug=$4
+  local targets=$5
+
+  local image_digest_file="docker-bake-digests.json"
+  local cmd=("docker" "buildx" "bake")
+
+  if [ "$push" = true ]; then
+    cmd+=("--push")
+  else
+    cmd+=("--load")
+  fi
+
+  if [ "$debug" = true ]; then
+    cmd+=("--progress" "plain")
+  fi
+
+  cmd+=("--metadata-file" $image_digest_file)
+  cmd+=("--file" "-")
+
+  # Add all targets to command
+  for target in $targets; do
+    if [ -n "$target" ]; then
+      # Transform target if it contains colon and dot
+      if [[ "$target" == *:* ]]; then
+        target=$(echo "$target" | sed 's/:/-/g' | sed 's/\./_/g')
+      fi
+      cmd+=("$target")
+    fi
+  done
+
+  echo "INFO: Building image: ${cmd[*]}" >&2
+  echo "$bakefile" | "${cmd[@]}"
+
+
+  if [ -f $image_digest_file ] && [ "$sign" = true ]; then
+    for key in $(jq -r 'keys[]' $image_digest_file); do
+      local image_digest=$(jq -r --arg key "$key" '.[$key]["containerimage.digest"]' $image_digest_file)
+      local image_name=$(jq -r --arg key "$key" '.[$key]["image.name"]' $image_digest_file)
+      if [ -n "$image_digest" ] && [ -n "$image_name" ]; then
+        local digest_tag="${image_name}@${image_digest}"
+        echo "INFO: Signing image: $digest_tag" >&2
+        sign_image $digest_tag $push
+      fi
+    done
+  fi
+}
+
+
+# Get docker bake file using project.yaml and versions.yaml in product path
+# The bake file is a JSON object
+# Arguments:
+#   $1: str, platforms, platforms for bakefile. eg: '["linux/amd64", "linux/arm64"]'
+# Returns:
+#   JSON: bake file JSON object
+#
+function get_bakefile () {
+  local platforms=$1
+
+  # if platforms is empty, set default value
+  if [ -z "$platforms" ]; then
+    platforms='["linux/'$(uname -m)'"]'
+  fi
+
+  local current_sha=$(git rev-parse HEAD)
+
+  # Use yq to parse yaml file to json
+  local products=$(yq eval '.products' -o=json project.yaml)
+  local bakefile=$(jq -n '{group:{}, target: {}}')
+  local default_groups=$(jq -n '[]')
+
+  for product_name in $(echo "$products" | jq -r '.[]'); do
+    local versions_file="${product_name}/versions.yaml"
+    echo "Product versions file: $versions_file" >&2
+    if [ -f $versions_file ]; then
+
+      # add product to default_groups
+      default_groups=$(echo "$default_groups" | jq --arg name "$product_name" '. + [$name]')
+
+      local product_groups=$(jq -n '[]')
+
+      local versions=$(yq eval '.versions' -o=json $versions_file)
+      local targets=$(jq -n '{}')
+      for version in $(echo "$versions" | jq -c '.[]'); do
+
+        # Construct bakefile target object
+        local target=$(jq -n '{}')
+
+        local product_version=$(echo "$version" | jq -r '.product')
+        # Transform target name, eg: kubedoop-0_0_0_dev
+        local target_name="${product_name}-$(echo "$product_version" | tr '.' '_')"
+        # Add target to product groups
+        product_groups=$(echo "$product_groups" | jq --arg name "$target_name" '. + [$name]')
+
+        local tags=$(jq -n --arg tag "$REGISTRY/$product_name:$product_version-$KUBEDOOP_TAG" '[$tag]')
+
+        # Construct contexts object
+        local contexts=$(jq -n '{}')
+
+        # Construct args object
+        local args=$(jq -n '{}')
+        for dependency in $(echo "$version" | jq -r 'keys[]'); do
+          # get value by dependency key
+          value=$(echo "$version" | jq -r --arg k "$dependency" '.[$k]')
+          # When dependency is in products, it is a context for target, should:
+          #  - Add to contexts
+          if echo "$products" | jq -e --arg k "$dependency" '. | index($k)' > /dev/null; then
+            local contexts_target_key="zncdatadev/image/$dependency"
+            local contexts_target_value="target:$dependency-$(echo $value | tr '.' '_')"
+            contexts=$(echo "$contexts" | jq --arg k "$contexts_target_key" --arg v "$contexts_target_value" '. + {($k): $v}')
+          fi
+
+          # Transform dependency name to uppercase and replace - with _, append _VERSION
+          # eg: INOTIFY_TOOLS_VERSION
+          local transformed_key=$(echo "$dependency" | tr '[:lower:]' '[:upper:]' | tr '-' '_')"_VERSION"
+          # append dependency to 'args'
+          args=$(echo "$args" | jq --arg k "$transformed_key" --arg v "$value" '. + {($k): $v}')
+        done
+
+        target=$(echo "$target" | jq --arg k "args" --argjson v "$args" '. + {($k): $v}')
+        target=$(echo "$target" | jq --arg k "platforms" --argjson v "$platforms" '. + {($k): $v}')
+        target=$(echo "$target" | jq --arg k "tags" --argjson v "$tags" '. + {($k): $v}')
+        target=$(echo "$target" | jq --arg k "context" --arg v "$product_name" '. + {($k): $v}')
+        target=$(echo "$target" | jq --arg k "dockerfile" --arg v "Dockerfile" '. + {($k): $v}')
+        # If contexts is not empty, add to target
+        if [ "$contexts" != "{}" ]; then
+          target=$(echo "$target" | jq --arg k "contexts" --argjson v "$contexts" '. + {($k): $v}')
+        fi
+
+        local cache_from="type=registry,ignore-err=true,mode=max,ref=$REGISTRY/$product_name:$product_version-kubedoop0.0.0-dev"
+        target=$(echo "$target" | jq --arg k "cache-from" --arg v "$cache_from" '. + {($k): [$v]}')
+
+        local datetime=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+        local labels=$(jq -n '{}')
+        labels=$(echo "$labels" | jq --arg k "org.opencontainers.image.title" --arg v "$product_name" '. + {($k): $v}')
+        labels=$(echo "$labels" | jq --arg k "org.opencontainers.image.version" --arg v "$product_version" '. + {($k): $v}')
+        labels=$(echo "$labels" | jq --arg k "org.opencontainers.image.created" --arg v "$datetime" '. + {($k): $v}')
+        labels=$(echo "$labels" | jq --arg k "org.opencontainers.image.revision" --arg v "$current_sha" '. + {($k): $v}')
+        target=$(echo "$target" | jq --argjson v "$labels" '. + {labels: $v}')
+
+        local annotations=$(jq -n '[]')
+        annotations=$(echo "$annotations" | jq --arg k "org.opencontainers.image.created=$datetime" '. + [$k]')
+        annotations=$(echo "$annotations" | jq --arg k "org.opencontainers.image.revision=$current_sha" '. + [$k]')
+        target=$(echo "$target" | jq --argjson v "$annotations" '. + {annotations: $v}')
+
+        # Add target to targets
+        targets=$(echo "$targets" | jq --arg k "$target_name" --argjson v "$target" '. + {($k): $v}')
+
+      done
+    fi
+    # Add product groups to group
+    bakefile=$(echo "$bakefile" | jq --arg k "$product_name" --argjson v "$product_groups" '.group |= . + {($k): {targets: $v}}')
+
+    # Add targets to bakefile
+    bakefile=$(echo "$bakefile" | jq --argjson v "$targets" '.target |= . + $v')
+  done
+
+  # Add default groups to bakefile
+  bakefile=$(echo "$bakefile" | jq --arg k "default" --argjson v "$default_groups" '.group |= . + {($k): {targets: $v}}')
+
+  # If CI_DEBUG is true, save bakefile to /tmp/bakefile.json
+  if [ "$CI_DEBUG" = "true" ]; then
+    jq -r '.' <<< $bakefile > /tmp/bakefile.json
+  fi
+
+  echo $(jq -c '.' <<< $bakefile)
+}
+
+
 main "$@"
